ce0e670007fad968993bc255f3d6121236b6b19a
skyzhma <skyzhma@gmail.com>
2023-06-20 00:33:58 -0700

    simple implementation of aggregration executor

--- src/execution/aggregation_executor.cpp
+++ src/execution/aggregation_executor.cpp
@@ -18,11 +18,31 @@ namespace bustub {
 
 AggregationExecutor::AggregationExecutor(ExecutorContext *exec_ctx, const AggregationPlanNode *plan,
                                          std::unique_ptr<AbstractExecutor> &&child)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_(std::move(child)),
+            aht_(SimpleAggregationHashTable(plan_->GetAggregates(), plan_->GetAggregateTypes())) {    
+    
+        aht_iterator_ = aht_.Begin();
+
+        Tuple *tuple;
+        RID *rid;
+
+        while (child->Next(tuple, rid)) {
+            aht_.InsertCombine(MakeAggregateKey(tuple), MakeAggregateValue(tuple));
+        }
+
+    }
 
 void AggregationExecutor::Init() {}
 
-auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+
+    if (aht_iterator_ == aht_.End()) {
+        return false;
+    }
+
+    
+
+ }
 
 auto AggregationExecutor::GetChildExecutor() const -> const AbstractExecutor * { return child_.get(); }
 
